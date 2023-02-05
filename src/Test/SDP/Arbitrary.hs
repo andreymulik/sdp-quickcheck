95ad9ed0c04ff9e06055e7e847749c22e077da2c
Andrey Mulik <work.a.mulik@gmail.com>
2023-02-05 10:13:00 +0300

    feat,refactor: Arbitrary1 instances, new AnyChunks arbitrary implementation, .cabal refactoring

--- src/Test/SDP/Arbitrary.hs
+++ src/Test/SDP/Arbitrary.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE MagicHash, TypeOperators, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE TypeOperators, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE Safe, CPP, MagicHash #-}
+
+#ifdef SDP_QUICKCHECK_QUALIFIED_CONSTRAINTS
+{-# LANGUAGE QuantifiedConstraints #-}
+#endif
 
 {- |
     Module      :  Test.SDP.Arbitrary
-    Copyright   :  (c) Andrey Mulik 2020
+    Copyright   :  (c) Andrey Mulik 2020-2023
     License     :  BSD-style
     Maintainer  :  work.a.mulik@gmail.com
     Portability :  non-portable (GHC extensions)
@@ -15,7 +20,7 @@ module Test.SDP.Arbitrary
   -- * Exports
   module Test.QuickCheck,
   
-  SArray#, SBytes#, AnyBorder (..), AnyChunks
+  SArray#, SBytes#, AnyBorder, AnyChunks
 )
 where
 
@@ -36,30 +41,65 @@ default ()
 
 instance Arbitrary E where arbitrary = return E
 
-instance (Arbitrary i, Arbitrary i') => Arbitrary (i' :& i)
+instance (Arbitrary i, Arbitrary is) => Arbitrary (is :& i)
   where
     -- [internal]: QuickCheck-2.10 doesn't provide 'applyArbitrary2'
     arbitrary = liftA2 (:&) arbitrary arbitrary
 
 --------------------------------------------------------------------------------
 
-instance (Arbitrary e) => Arbitrary (SArray# e)
+instance Arbitrary e => Arbitrary (SArray# e)
   where
-    arbitrary = fromList <$> arbitrary
+    arbitrary = do n <- getSize; fromListN n <$> vector n
 
 instance (Unboxed e, Arbitrary e) => Arbitrary (SBytes# e)
   where
     arbitrary = fromList <$> arbitrary
 
+instance (Nullable1 rep e, Arbitrary (rep e)) => Arbitrary (AnyChunks rep e)
+  where
+    arbitrary = do n <- getSize; fromChunks <$> vectorOf n arbitrary
+
+instance (Index i, Estimate1 rep e, Arbitrary (rep e)) => Arbitrary (AnyBorder rep i e)
+  where
+    arbitrary = do
+      es <- arbitrary
+      let (l, u) = defaultBounds (sizeOf es)
+      return (AnyBorder l u es)
+
 --------------------------------------------------------------------------------
 
--- TODO: rewrite as arbitrary chunk list generator (needs sdp improvements).
+#if MIN_VERSION_QuickCheck(2,10,0)
 
-instance (Bordered1 rep Int e, Linear1 rep e, Arbitrary e) => Arbitrary (AnyChunks rep e)
+-- | Since @QuickCheck-2.10@.
+instance Arbitrary1 SArray#
   where
-    arbitrary = fromList <$> arbitrary
+    liftArbitrary gen = do n <- getSize; fromListN n <$> vectorOf n gen
 
-instance (Index i, Bordered1 rep Int e, Arbitrary (rep e)) => Arbitrary (AnyBorder rep i e)
+#ifdef SDP_QUICKCHECK_QUALIFIED_CONSTRAINTS
+-- | Since @QuickCheck-2.10@, require @GHC 8.6+@.
+instance (Nullable' rep, Arbitrary1 rep) => Arbitrary1 (AnyChunks rep)
   where
-    arbitrary = (\ es -> uncurry AnyBorder (defaultBounds $ sizeOf es) es) <$> arbitrary
+    liftArbitrary gen = do n <- getSize; fromChunks <$> vectorOf n (liftArbitrary gen)
+
+-- | Since @QuickCheck-2.10@, require @GHC 8.6+@.
+instance (Index i, Nullable' rep, Estimate' rep, Arbitrary1 rep)
+      => Arbitrary1 (AnyBorder rep i)
+  where
+    liftArbitrary gen = do
+      es <- liftArbitrary gen
+      let (l, u) = defaultBounds (sizeOf es)
+      return (AnyBorder l u es)
+#endif
+
+#else
+
+{-
+  getSize available since QuickCheck-2.10, for QuickCheck-2.9 I only can
+  generate an arbitrary size
+-}
+getSize :: Gen Int
+getSize =  abs <$> arbitrary
+
+#endif
 
